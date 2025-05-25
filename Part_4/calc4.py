bb61f55b3221a8dbd95e6360ccf178c0382dab85
Ray <ray.dreamer400@gmail.com>
2025-05-25 15:48:22 +0530

    calc4

---
+++ Part_4/calc4.py
@@ -0,0 +1,150 @@
+# Variables representing the token types (kinda like data types???)
+INTEGER, PLUS, MINUS, MUL, DIV, EOF = "INTEGER", "PLUS", "MINUS", "MUL", "DIV", "EOF"
+
+
+
+# The token class
+class Token(object):
+    def __init__(tkn, type, value):
+        tkn.type = type
+        tkn.value = value
+    
+    
+    def __str__(tkn):
+        return f"Token({tkn.type}, {tkn.value})"
+    
+    
+    def __repr__(tkn):
+        return tkn.__str__()
+
+
+
+class Lexer(object):
+    #############################################################
+    # LEXER CODE                                                #
+    #############################################################
+    def __init__(intr, text):
+        intr.text = text
+        intr.pos = 0
+        intr.current_char = intr.text[intr.pos]
+        intr.current_token = None
+    
+    
+    def error(intr):
+        raise Exception("Invalid Syntax!!!")
+    
+    
+    def advance(intr):
+        intr.pos += 1
+        if intr.pos > len(intr.text) - 1:
+            intr.current_char = None
+        else:
+            intr.current_char = intr.text[intr.pos]
+    
+    
+    def skip_whitespace(intr):
+        while intr.current_char is not None and intr.current_char.isspace():
+            intr.advance()
+    
+    
+    def integer(intr):
+        result = ""
+        
+        while intr.current_char is not None and intr.current_char.isdigit():
+            result += intr.current_char
+            intr.advance()
+        
+        return int(result)
+    
+    
+    def get_next_token(intr):
+        while intr.current_char is not None:
+            if intr.current_char.isspace():
+                intr.skip_whitespace()
+                continue
+            
+            if intr.current_char.isdigit():
+                return Token(INTEGER, intr.integer())
+            
+            if intr.current_char == "+":
+                intr.advance()
+                return Token(PLUS, "+")
+            
+            if intr.current_char == "-":
+                intr.advance()
+                return Token(MINUS, "-")
+            
+            if intr.current_char == "*":
+                intr.advance()
+                return Token(MUL, "*")
+            
+            if intr.current_char == "/":
+                intr.advance()
+                return Token(DIV, "/")
+            
+            intr.error()
+        
+        return Token(EOF, None)
+    
+    
+    #############################################################
+    # PARSER CODE                                               #
+    #############################################################
+
+
+
+class Interpreter(object):
+    def __init__(intr, text):
+        intr.lexer = Lexer(text)
+        intr.current_token = intr.lexer.get_next_token()
+    
+    def eat(intr, token_type):    
+        if intr.current_token.type == token_type:
+            intr.current_token = intr.lexer.get_next_token()
+        else:
+            intr.lexer.error()
+    
+    
+    def term(intr):
+        value = intr.current_token.value
+        intr.eat(INTEGER)
+        return value
+
+
+    def expr(intr):
+        result = intr.term()
+        
+        while intr.current_token.type in (MUL, DIV, PLUS, MINUS):
+            if intr.current_token.type == MUL:
+                intr.eat(MUL)
+                result *= intr.term()
+            elif intr.current_token.type == DIV:
+                intr.eat(DIV)
+                result /= intr.term()
+            elif intr.current_token.type == PLUS:
+                intr.eat(PLUS)
+                result += intr.term()
+            else:
+                intr.eat(MINUS)
+                result -= intr.term()
+        
+        return result
+
+
+
+def main():
+    while True:
+        try:
+            text = input("calc> ")
+        except EOFError:
+            break
+        if not text:
+            continue
+        interpreter = Interpreter(text)
+        result = interpreter.expr()
+        print(result)
+
+
+
+if __name__ == "__main__":
+    main()
