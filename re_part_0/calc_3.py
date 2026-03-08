f5c0412e1c9bacd390ad0d98bdcf171020fef659
Ray <ray.dreamer400@gmail.com>
2026-03-08 20:03:16 +0530

    calc part 3 completed

---
+++ re_part_0/calc_3.py
@@ -0,0 +1,102 @@
+INTEGER, PLUS, MINUS, EOF = "INTEGER", "PLUS", "MINUS", "EOF"
+
+class Token(object):
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+    
+    def __str__(self):
+        return f"Token({self.type}, {self.value})"
+    
+    def __repr__(self):
+        return self.__str__()
+    
+
+
+class Interpreter(object):
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+        self.current_token = None
+    
+    def error(self):
+        raise Exception("Invalid Syntax")
+    
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
+    
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+    
+    def integer(self):
+        result = ""
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+    
+    def get_next_token(self):
+        while self.current_char is not None:
+            if self.current_char.isspace():
+                self.skip_whitespace()
+            if self.current_char.isdigit():
+                return Token(INTEGER, self.integer())
+            if "+" == self.current_char:
+                self.advance()
+                return Token(PLUS, "+")
+            if "-" == self.current_char:
+                self.advance()
+                return Token(MINUS, "-")
+            self.error()
+        return Token(EOF, None)
+    
+    def eat(self, token_type):
+        if token_type == self.current_token.type:
+            self.current_token = self.get_next_token()
+        else:
+            self.error()
+    
+    def term(self):
+        value = self.current_token.value
+        self.eat(INTEGER)
+        return value
+    
+    def expr(self):
+        self.current_token = self.get_next_token()
+        result = self.current_token.value
+        self.eat(INTEGER)
+        
+        while self.current_token.type in (PLUS, MINUS):
+            if PLUS == self.current_token.type:
+                self.eat(PLUS)
+                result += self.term()
+            else:
+                self.eat(MINUS)
+                result -= self.term()
+        
+        return result
+
+
+
+def main():
+    while True:
+        try:
+            text = input("calc>")
+        except EOFError:
+            break
+        if not text:
+            continue
+        interpreter = Interpreter(text)
+        result = interpreter.expr()
+        print(result)
+
+
+
+if __name__ == "__main__":
+    main()
